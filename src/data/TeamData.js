e83bee0d061a1b1476adba6a56cacaf6489b2d45
Iron Man <109871776+Ironman3580@users.noreply.github.com>
2025-09-18 12:25:06 +0530

    Update TeamData.js

--- src/data/TeamData.js
+++ src/data/TeamData.js
@@ -1,32 +1,27 @@
 
 export const TeamData = [
-  {
-      id: 1,
-      name: 'Pradnya Kakade',
-      date: '16 Dec 2005',
-      domain: 'Founder and CEO',
-      description: 'Cheif Executive Officer'
-    },
+
     {
-      id: 2,
+      id: 1,
       name: 'Jaydeep Navghade',
       date: '02 oct 2006',
       domain: 'Technical and R&D',
       description: 'Technical and R&D Department Head'
     },
     {
-      id: 3,
+      id: 2,
       name: 'Saistavananjali Kute',
       date: 'July 18, 2006',
       domain: 'Public Realtion Head',
       description: 'Public Realtion and Marketing Head.'
     },
     {
-      id: 4,
+      id: 3,
       name: 'Prathmesh Anantwar',
       date: 'December 10, 2023',
       domain: 'strategic planning',
       description: 'Strategic Planning or the organization'
     },
   
+
 ];
